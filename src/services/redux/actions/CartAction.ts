305ba549f8ef9255f9a255f579cc3c9805e9fcc8
anandsingh111 <anandsingh181097@gmail.comm>
2024-02-08 18:20:17 +0530

    redux commit

--- src/services/redux/actions/CartAction.ts
+++ src/services/redux/actions/CartAction.ts
@@ -1,6 +1,10 @@
 const types = {
   ADD_ITEM_TO_CART: 'ADD_ITEM_TO_CART',
+  CUSTOMIZATION_IN : "CUSTOMIZATION_IN",
+  ADDITEM_IN : "ADDITEM_IN",
   REMOVE_ITEM_FROM_CART: 'REMOVE_ITEM_FROM_CART',
+  ORDERDETAILS_IN : "ORDERDETAILS_IN",
+  ITEMUPDATE_IN_MENUITEM :"ITEMUPDATE_IN_MENUITEM",
 };
 
 const addItemToCart = (cartItem: any) => {
@@ -16,9 +20,43 @@ const removeItems = () => {
     payload: [],
   };
 };
+//  const additemIn = (data:any) => {
+//   return {
+//     type: types.ADDITEM_IN,
+//     payload: data,
+//   };
+// };
+//  const customizationIn = (data:any) => {
+//   return {
+//     type: types.CUSTOMIZATION_IN,
+//     payload: data,
+//   };
+// };
+ const sameitemupdateIn = (qty:any) => {
+  return {
+    type: types.ITEMUPDATE_IN_MENUITEM,
+    payload: {
+      item: qty,
+    },
+  };
+};
+ const orderdetailsIn = (data:any) => {
+  return {
+    type: types.ORDERDETAILS_IN,
+    payload: {
+      id: Math.random(),
+      data: data,
+    },
+  };
+};
+
 
 export default {
   addItemToCart,
   types,
   removeItems,
+  additemIn,
+  customizationIn,
+  sameitemupdateIn,
+  orderdetailsIn,
 };
--- src/services/redux/reducers/CartReducer.ts
+++ src/services/redux/reducers/CartReducer.ts
@@ -4,7 +4,7 @@ const initialState = {
   cartItems: [],
 };
 
-const CartReducer = (state = initialState, action: any) => {
+const CartReducer = (state = initialState, action: any,payload:any) => {
   switch (action.type) {
     case CartAction.types.ADD_ITEM_TO_CART:
       return {
@@ -16,6 +16,48 @@ const CartReducer = (state = initialState, action: any) => {
         ...state,
         cartItems: action.payload,
       };
+
+      case CartAction.types.ITEMUPDATE_IN_MENUITEM:
+      const { item } = action.payload;
+ 
+      const itemobjIndex = state.cartItems.findIndex(
+        (obj:any) => obj.responseOrder.id === item.responseOrder.id
+        //  &&
+        // ((item.spicy && obj.data.customFieldListItem === item.spicy) ||
+        //   (!item.spicy && !obj.data.customFieldListItem)) &&
+        // (!item.subItem ||
+        //   (obj.data.subItem && obj.data.subItem.id === item.subItem.id)) &&
+        // (item.addons.length === 0 ||
+        //   (obj.data.addons.length !== 0 &&
+        //     compareArrays(obj.data.addons, item.addons)))
+      );
+ 
+      state.cartItems[itemobjIndex].qty += item.qty;
+      state.cartItems[itemobjIndex].addonsamount1 =
+        state.cartItems[itemobjIndex].addonsamount1 + item?.addonsamount1;
+      return {
+        ...state,
+        
+        cartItems: [...state.cartItems],
+       
+ 
+        // updatedResponse : data1.responseOrder
+      };
+      case CartAction.types.ORDERDETAILS_IN:
+      const { id, data } =  action.payload;
+      return {
+        ...state,
+ 
+        cartItems: [
+          ...state.cartItems,
+          {
+            id: id,
+            data: data,
+          },
+        ],
+      
+      };
+
     default:
       return state;
   }
